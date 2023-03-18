5028fa696d33d245548399ca6c715c21a6534f25
tpalinski <tpalinski@icloud.com>
2023-03-18 21:52:42 +0100

    Add db driver

--- src/main/java/com/bondzio/bondziosshed/controllers/ApiRoutes.java
+++ src/main/java/com/bondzio/bondziosshed/controllers/ApiRoutes.java
@@ -1,20 +1,23 @@
 package com.bondzio.bondziosshed.controllers;
 
 
+import com.bondzio.bondziosshed.utils.DBDriver;
 import com.bondzio.bondziosshed.utils.Room;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiRoutes {
 
+    static DBDriver driver = new DBDriver();
     @GetMapping("/")
     public String homepage(){
         return "Refer to the documentation for all routes";
     }
 
     @GetMapping("/room")
-    public Room getRoom(){
-        return new Room("name", "passw8rd");
+    public Room getRoom(@RequestParam(name = "id") String roomId){
+        return new Room(roomId, "Password");
     }
 }
--- src/main/java/com/bondzio/bondziosshed/utils/DBDriver.java
+++ src/main/java/com/bondzio/bondziosshed/utils/DBDriver.java
@@ -1,5 +1,33 @@
 package com.bondzio.bondziosshed.utils;
 
-public class DBDriver {
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import io.github.cdimascio.dotenv.Dotenv;
 
+public class DBDriver {
+    private Dotenv env;
+    private String password;
+    private ConnectionString connectionString;
+    private MongoClientSettings settings;
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+
+    public DBDriver(){
+        env = Dotenv.configure().load();
+        this.connectionString = new ConnectionString("mongodb+srv://admin:" + env.get("DB_PASSWORD") + ">@cluster0.ipgs6c8.mongodb.net/?retryWrites=true&w=majority");
+        this.settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+        this.mongoClient = MongoClients.create(settings);
+        this.database = mongoClient.getDatabase("kalambury");
+    }
 }
